6e5a1feaebacfbfa2b25f40ac6741668098e83a0
Jerry Chen <jerryc443@gmail.com>
2026-04-22 20:56:00 -0700

    upd

--- src/utils.ts
+++ src/utils.ts
@@ -1,6 +1,7 @@
 import pc from 'picocolors'
 
-export const begin = (s: string) => console.log(pc.blue(`⏳ ${s}`))
+export const begin = (s: string) =>
+  console.log(pc.underline(pc.blue(`⏳ ${s}`)))
 
 export const done = () =>
   console.log(pc.bold(pc.underline(pc.green('🎉 DONE!'))))
