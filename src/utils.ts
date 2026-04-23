4c1d75f4aec88d1858971f6fce21f970163b48c1
Jerry Chen <jerryc443@gmail.com>
2026-04-22 20:50:04 -0700

    upd

--- src/utils.ts
+++ src/utils.ts
@@ -6,7 +6,7 @@ export const done = () =>
   console.log(pc.bold(pc.underline(pc.green('🎉 DONE!'))))
 
 export const fail = (s: string) =>
-  console.log(pc.bold(pc.underline(pc.bgRed(`\n❌ ${s}`))))
+  console.log(`\n❌ ${pc.bold(pc.underline(pc.bgRed(s)))}`)
 
 export const success = (s: string) =>
   console.log(pc.bold(pc.underline(pc.green(`✅ ${s}`))))
