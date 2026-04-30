6149afe6a6b51c68af74ca340c5b863924c7f1fd
Jerry Chen <jerry.chen1@bytedance.com>
2026-04-30 15:47:01 -0700

    upd

--- src/utils/format-date.ts
+++ src/utils/format-date.ts
@@ -1,11 +1,7 @@
 export function formatDate(date: ConstructorParameters<typeof Date>[0]) {
-  return new Intl.DateTimeFormat('zh-CN', {
-    year: 'numeric',
-    month: '2-digit',
-    day: '2-digit',
-    hour: '2-digit',
-    minute: '2-digit',
-    second: '2-digit',
-    timeZoneName: 'short',
-  }).format(new Date(date))
+  const d = new Date(date)
+  const abbr = new Intl.DateTimeFormat('en-US', { timeZoneName: 'short' })
+    .formatToParts(d)
+    .find(p => p.type === 'timeZoneName')?.value
+  return `${d.toLocaleDateString('zh-CN')} ${d.toTimeString().split(' ')[0]} ${abbr ? `(${abbr})` : ''}`
 }
