c66cd8f8f60df1b61b21f697d586343a733507ed
Jerry Chen <jerryc443@gmail.com>
2026-04-30 19:59:44 -0700

    upd

--- src/utils/format-date.ts
+++ src/utils/format-date.ts
@@ -1,7 +1,8 @@
 export function formatDate(date: ConstructorParameters<typeof Date>[0]) {
   const d = new Date(date)
-  const abbr = new Intl.DateTimeFormat('en-US', { timeZoneName: 'short' })
+  const abbr = new Intl.DateTimeFormat('en-US', { timeZoneName: 'longOffset' })
     .formatToParts(d)
-    .find(p => p.type === 'timeZoneName')?.value
-  return `${d.toLocaleDateString('zh-CN')} ${d.toTimeString().split(' ')[0]} ${abbr ? `(${abbr})` : ''}`
+    .find(p => p.type === 'timeZoneName')
+    ?.value.replace('GMT', '')
+  return `${d.toLocaleDateString('zh-CN')} ${d.toTimeString().split(' ')[0]} ${abbr ?? ''}`
 }
