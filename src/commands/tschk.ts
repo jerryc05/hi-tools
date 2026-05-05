e3ed977e1160e61b853fd6d17802d0641ecc087d
Jerry Chen <jerry.chen1@bytedance.com>
2026-05-04 22:45:30 -0700

    upd

--- src/commands/tschk.ts
+++ src/commands/tschk.ts
@@ -6,6 +6,8 @@ export default [
     command: 'tschk',
     describe: 'My ts-check rules',
     handler: async () => {
+      // todo ,,, support tsgo
+
       const s = spinner()
       s.start('Running custom tsc type-check...')
 
@@ -25,7 +27,7 @@ export default [
         const { stdout } = await execa({
           reject: false, // 报错时不直接抛出异常
           stderr: 'ignore',
-        })`npx tsc --noEmit --emitDeclarationOnly false`
+        })`npx tsc --build --noEmit --emitDeclarationOnly false`
 
         const lines = stdout.split('\n').filter(line => {
           if (line.includes('/node_modules/')) return false
