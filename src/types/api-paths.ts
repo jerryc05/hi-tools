058a05f089b44f54a2a2016ab0ef56cee995fc2a
Jerry Chen <jerry.chen1@bytedance.com>
2026-05-07 18:55:07 -0700

    upd

--- src/types/api-paths.ts
+++ src/types/api-paths.ts
@@ -2,7 +2,7 @@ import base62 from 'base62'
 
 export namespace API_PATH {
   let i = 0
-  export const RAX_WEBPAGE = `/${base62.encode(i++)}`
+  export const RAX_WEBPAGE = '/'
   export const RAX_DEVICE_INFO = `/${base62.encode(i++)}`
   export const RAX_SCRSHOT = `/${base62.encode(i++)}`
   export const RAX_OPEN_SCHEMA = `/${base62.encode(i++)}`
