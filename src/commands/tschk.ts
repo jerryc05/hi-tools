32d738ff80aa60f22f03d07d933f48d2cb8726ea
Jerry Chen <jerry.chen1@bytedance.com>
2026-05-11 17:52:27 -0700

    0.2.3

--- src/commands/tschk.ts
+++ src/commands/tschk.ts
@@ -6,27 +6,27 @@ export default [
     command: 'tschk',
     describe: 'My ts-check rules',
     handler: async () => {
-      // todo ,,, support tsgo
-
-      const s = spinner()
-      s.start('Running custom tsc type-check...')
-
-      // 需要忽略的错误码列表
-      const ignoredCodes = [
-        '2322', // Type 'X' is not assignable to type 'Y'
-        '2551', // Property 'X' does not exist on type 'Y'. Did you mean 'Z'?
-        '6133', // 'X' is declared but its value is never read (Unused var)
-        '6192', // All imports in 'X' are unused
-        '18048', // 'X' is possibly 'null' or 'undefined'
-      ]
-
       const { execa } = await import('execa')
 
-      try {
+      {
         const { stdout } = await execa({
           reject: false, // 报错时不直接抛出异常
           stderr: 'ignore',
         })`npx tsc --build --noEmit --emitDeclarationOnly false`
+
+        // todo ,,, support tsgo
+
+        const s = spinner()
+        s.start('Running custom tsc type-check...')
+
+        // 需要忽略的错误码列表
+        const ignoredCodes = [
+          '2322', // Type 'X' is not assignable to type 'Y'
+          '2551', // Property 'X' does not exist on type 'Y'. Did you mean 'Z'?
+          '6133', // 'X' is declared but its value is never read (Unused var)
+          '6192', // All imports in 'X' are unused
+          '18048', // 'X' is possibly 'null' or 'undefined'
+        ]
 
         const lines = stdout.split('\n').filter(line => {
           if (line.includes('/node_modules/')) return false
@@ -39,17 +39,29 @@ export default [
         })
 
         if (lines.length > 0) {
-          s.stop('Type-check failed')
           lines.map(l => log.error(l))
+          s.stop('Type-check failed')
           process.exit(1)
         } else {
           s.stop('Type-check passed')
         }
-      } catch (err) {
-        s.stop('Type-check failed')
-        console.error(err)
-        log.error('Type-check failed')
-        process.exit(1)
+      }
+
+      {
+        const s = spinner()
+        s.start('Running knip deps check...')
+
+        const { stdout, exitCode } = await execa({
+          reject: false, // 报错时不直接抛出异常
+          stderr: 'ignore',
+        })`pnpm dlx knip --strict --dependencies`
+
+        if (exitCode !== 0) {
+          log.error(stdout)
+          s.stop('Dep check failed')
+          process.exit(1)
+        }
+        s.stop('Dep check passed')
       }
     },
   },
