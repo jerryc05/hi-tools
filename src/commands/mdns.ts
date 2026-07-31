9aac3b502bca002b037d547fe6278328d023c823
Jerry Chen <jerry.chen1@bytedance.com>
2026-07-31 15:45:32 -0700

    upd

--- src/commands/mdns.ts
+++ src/commands/mdns.ts
@@ -1,4 +1,4 @@
-import { hostname } from 'node:os'
+import os from 'node:os'
 import type { HiCmd } from '@/types/cmd-module'
 
 export default [
@@ -6,9 +6,25 @@ export default [
     command: 'mdns',
     describe: 'Show mdns [hostname].local',
 
-    handler() {
-      const n = hostname()
-      console.log(n.toLowerCase().endsWith('.local') ? n : `${n}.local`)
+    async handler() {
+      let mdnsHostName = ''
+
+      if (os.platform() === 'darwin') {
+        try {
+          const { execa } = await import('execa')
+          mdnsHostName = (await execa`scutil --get LocalHostName`).stdout.trim()
+        } catch (e) {
+          console.error(e)
+        }
+      }
+
+      if (!mdnsHostName) mdnsHostName = os.hostname()
+
+      console.log(
+        mdnsHostName.toLowerCase().endsWith('.local') ?
+          mdnsHostName
+        : `${mdnsHostName}.local`,
+      )
     },
   },
 ] satisfies HiCmd[]
